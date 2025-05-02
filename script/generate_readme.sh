29b0c6a2560102e2b5504298a0f7d2a68819d6be
筒井雄太 <ydsikohsh@gmail.com>
2025-05-02 22:30:51 +0900

    modify readme

--- script/generate_readme.sh
+++ script/generate_readme.sh
@@ -7,13 +7,13 @@ cd "$(dirname "$0")/.." || exit 1
 find . -type d | while read -r dir; do
   # SVGが1つ以上存在するか
   svg_files=("$dir"/*.svg)
-  if compgen -G "$dir"/*.svg > /dev/null && [ ! -f "$dir/index.md" ]; then
-    echo "生成: $dir/index.md"
+  if compgen -G "$dir"/*.svg > /dev/null && [ ! -f "$dir/README.md" ]; then
+    echo "生成: $dir/README.md"
     {
       for svg in "${svg_files[@]}"; do
         filename=$(basename "$svg")
         echo "![${filename%.*}]($filename)"
       done
-    } > "$dir/index.md"
+    } > "$dir/README.md"
   fi
 done
